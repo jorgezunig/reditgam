290ad940fa2324e861bcc1a489068fcbaf62df25
jorgezunig <jorge.z.delacruz@gmail.com>
2015-05-22 20:38:10 -0500

    09. Se agrega un servicio del tipo factory para almacenar los posts

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,38 +1,62 @@
 var modulo1 = 
 	angular.module("reditgam",[]);
 
-modulo1.controller("mainCtrl",[
-	'$scope',
-	function($scope){
-		$scope.test = "Hola Angular";
-		// Modelo
-		$scope.posts = 
-		[{title: "Post 1", upvotes: 5},
-		 {title: "Post 2", upvotes: 15},
-		 {title: "Post 3", upvotes: 7},
-		 {title: "Post 4", upvotes: 9},
-		 {title: "Post 5", upvotes: 3},
-		 {title: "Post 6", upvotes: 5}];
+	// Creando un servicio del tipo factory
+modulo1.factory('posts',[function(){
+	// Cuerpo del factory llamado post
+	var o = {
+		posts : [
+		{	
+				title: "post 1", upvotes: 15,
+				comments: [
+					{author: "Karina", body:"Esto esta de pelos.",
+					upvotes:3},
+					{author: "Gamaliel", body:"Esto es basura.",
+					upvotes:0}]
+			},
+			{	
+				title: "post 2", upvotes: 4,
+				comments: [
+					{author: "Coco", body:"Esto es asombroso.",
+					upvotes:5},
+					{author: "Cristian", body:"Esto esta aburrido.",
+					upvotes:1}]
+			}
+		]
+	};
+	// Retronado objeto de datos persistentes
+	return o;
+}]);
 
-		 // Metodo del controlador
-		 $scope.addPost = function(){
-		 	if(!$scope.title || $scope.title === "")
-		 	{
-		 		alert("No se permite postear titulos vacios");
-		 		return;
-		 	}
+// Creando controlador	
+// dependcy injection
+
+modulo1.controller("mainCtrl",[
+		'$scope','posts', // Inyectando factory post
+	function($scope, posts){
+		$scope.test = "Hola Angular";
+		
+
+		  // Metodo del controlador
+ 		 $scope.addPost = function(){
+ 		 	if(!$scope.title || $scope.title === "")
+ 		 	{
+ 		 		alert("No se permite postear titulos vacios");
+ 		 		return;
+ 		 	}
  		 	$scope.posts.push(
-		 		{
-		 			title: $scope.title,
-		 			link: $scope.link,
-		 		 	upvotes: 0
-		 		 });
-		 	// Two-way data binding
-		 	$scope.link = "";
-		 };
-		 // Metodo que incrementa el voto
-		 // de un post en una unidad
-		 $scope.incrementUpvotes = function(post){
-		 	post.upvotes += 1;
-		 };
-	}]);
+ 		 		{
+ 		 			title: $scope.title,
+ 		 			link: $scope.link,
+ 		 		 	upvotes: 0
+ 		 		 });
+ 		 	// Two-way data binding
+ 		 	$scope.title = "";
+ 		 	$scope.link = "";
+ 		 };
+ 		 // Metodo que incrementa el voto
+ 		 // de un post en una unidad
+ 		 $scope.incrementUpvotes = function(post){
+ 		 	post.upvotes += 1;
+ 		 };
+ 	}]);
---
+++ ss
@@ -0,0 +1,53 @@
+[33mcommit 2f8bcbb0d6289993e3304a17f0c1e184baab88e3[m
+Author: jorgezunig <jorge.z.delacruz@gmail.com>
+Date:   Fri May 22 20:25:49 2015 -0500
+
+    08. Se estiliza la aplicacion y se cargan los scripts de Bootstrap y jquery.
+
+[33mcommit 7e945b50d10749ac677adea41d8b10704de1a5b9[m
+Author: jorgezunig <jorge.z.delacruz@gmail.com>
+Date:   Fri May 22 19:58:53 2015 -0500
+
+    07. Se agrega gitignore
+
+[33mcommit 7d1777a89d20de1e383aaf5348fbf98cf31c4fd0[m
+Author: jorgezunig <jorge.z.delacruz@gmail.com>
+Date:   Fri May 22 19:30:51 2015 -0500
+
+    06. Se agrega la funcionalidad de compartir links y se carga bootstrap con exito
+
+[33mcommit 7b89c1233080d2edef5986779ac5290b0e81cd8a[m
+Author: jorgezunig <jorge.z.delacruz@gmail.com>
+Date:   Fri May 22 19:23:54 2015 -0500
+
+    05. Se habilita la funcion de Post de forma automatica
+
+[33mcommit 8403d78dec9885076f09c1549dde857c93976dd7[m
+Author: jorgezunig <jorge.z.delacruz@gmail.com>
+Date:   Fri May 22 19:13:53 2015 -0500
+
+    04. Agregar nuevos post staticos
+
+[33mcommit a8faa17423a67b3ca887afc8f821f3e545e32e12[m
+Author: jorgezunig <jorge.z.delacruz@gmail.com>
+Date:   Fri May 22 19:07:23 2015 -0500
+
+    03. Post como objetos y ordenados
+
+[33mcommit bef282880c7d860fa5d135a475cd7760359488c2[m
+Author: jorgezunig <jorge.z.delacruz@gmail.com>
+Date:   Fri May 22 18:55:16 2015 -0500
+
+    02. Post con ng-repeat
+
+[33mcommit cb6052bff625a2a6cbec85044c04ffcd2649ec0a[m
+Author: jorgezunig <jorge.z.delacruz@gmail.com>
+Date:   Fri May 22 18:09:37 2015 -0500
+
+    0.1 Hola Mundo Angular
+
+[33mcommit 9ff0eff15974a5dd3af6bfd8894bb9ceab5b1339[m
+Author: jorgezunig <jorge.z.delacruz@gmail.com>
+Date:   Fri May 22 17:50:32 2015 -0500
+
+    Prueba
