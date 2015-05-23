a8faa17423a67b3ca887afc8f821f3e545e32e12
jorgezunig <jorge.z.delacruz@gmail.com>
2015-05-22 19:07:23 -0500

    03. Post como objetos y ordenados

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,17 +1,17 @@
 var modulo1 = 
 	angular.module("reditgam",[]);
-	
+
 modulo1.controller("mainCtrl",[
 	'$scope',
 	function($scope){
 		$scope.test = "Hola Angular";
 		// Modelo
 		$scope.posts = 
-		[{"Post 1",
-		  "Post 2",
-		  "Post 3",
-		  "Post 4",
-		  "Post 5",
-		  "Post 6"];
+		[{title: "Post 1", upvotes: 5},
+		 {title: "Post 2", upvotes: 15},
+		 {title: "Post 3", upvotes: 7},
+		 {title: "Post 4", upvotes: 9},
+		 {title: "Post 5", upvotes: 3},
+		 {title: "Post 6", upvotes: 5}];
 
 	}]);
